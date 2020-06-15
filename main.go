efeb6aa577c5109080f1f031217afdfc234dda2e
artsantana <artsantana1457@hotmail.com>
2020-06-14 23:34:37 -0300

    Now we have a package for handle the query and  middlewares for characters endpoit

--- main.go
+++ main.go
@@ -1,28 +1,24 @@
 package main
 
 import (
+	"charactersManager/character"
 	"fmt"
 	"log"
 	"net/http"
-
-	"charactersManager/global"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	user := global.Env("USER")
-	fmt.Println(user)
-
 	router := mux.NewRouter()
 	const port string = ":3000"
 	router.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprintln(res, "Hello World")
 	})
-	router.HandleFunc("/posts", getPosts).Methods("GET")
-	router.HandleFunc("/posts", addPost).Methods("POST")
+	router.HandleFunc("/characters", character.GetCharacters).Methods("GET")
 
 	log.Println("Server listening on port", port)
 	log.Fatalln(http.ListenAndServe(port, router))
+
 }
